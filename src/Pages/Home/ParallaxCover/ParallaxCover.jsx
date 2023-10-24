b0f2bcf9c79ff080393d3b4d2637dad80d0ce4da
Araf <arafathossain.baiust@gmail.com>
2023-10-24 11:52:59 +0600

    Home details added

--- src/Pages/Home/ParallaxCover/ParallaxCover.jsx
+++ src/Pages/Home/ParallaxCover/ParallaxCover.jsx
@@ -3,6 +3,9 @@ import b from '../../../assets/home/b.jpg'
 import { Link } from 'react-router-dom';
 
 const ParallaxCover = () => {
+    const scrollToTop = () => {
+        window.scrollTo(0, 0)
+    }
     return (
         <div className='mt-10'>
             <Parallax
@@ -18,7 +21,7 @@ const ParallaxCover = () => {
                             <h1 className="mb-4 text-3xl font-bold uppercase">Why BAIUST?</h1>
                             <p className="mb-2"><b><u>Army Administered & UGC Approved:</u></b> Bangladesh Army International University of Science and Technology (BAIUST) runs under the direct supervision of the Bangladesh Army. Its programs are approved by UGC. The Vice-Chancellor (VC) is selected from the teachers of the Military Institute of Science & Technology (MIST) and approved by the Chancellor— the Honorable President of Bangladesh.
                                 <br /><b><u>Access to the VC:</u></b> The University gives top priority to its students and provides utmost care to students’ needs. All the students have access to the VC for their educational, administrative, and personal requirements when necessary. In addition, the VC interacts with all students regularly. The VC and faculties also interact with parents/guardians when required. </p>
-                                <button className="btn btn-outline btn-info"><Link to='/readMore'>Read More</Link></button>
+                            <Link onClick={scrollToTop} className="btn btn-outline btn-info"  to='/readMore'>Read More</Link>
                         </div>
                     </div>
                 </div>
