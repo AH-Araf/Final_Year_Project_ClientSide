367c41296e6e706b3bccf7e2551725e91890a695
Araf <arafathossain.baiust@gmail.com>
2023-11-03 19:36:13 +0600

    Changed

--- src/components/FacultyBody/FacultyBody.jsx
+++ src/components/FacultyBody/FacultyBody.jsx
@@ -10,9 +10,9 @@ const FacultyBody = ({ img,
     workExperience,
 }) => {
     return (
-        <div className="hover:border-4 hover:border-green-100 md:m-4 m-2 bg-blue-50 " data-aos="fade-up">
+        <div className="hover:px-1 border-2 border-blue-100 md:m-4 m-2 bg-blue-50 " data-aos="fade-up">
             <div className="mx-auto w-10/12 text-center my-8">
-                <div className="flex justify-center items-center" ><img className="w-32 h-38 border-2 border-gray-300" src={img} alt="" /></div>
+                <div className="flex justify-center" ><img className="w-32 h-40 border-2 border-gray-300" src={img} alt="" /></div>
                 <p className="text-yellow-600">{designation}</p>
                 <p className="text-yellow-600 mb-2">BAIUST, Cumilla</p>
                 <h3 className="lg:text-xl uppercase border-y-4 ">{name}</h3>
