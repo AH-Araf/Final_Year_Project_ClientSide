bc30e9935010759e442cd08022bc3bcdb2c6a6d7
Araf <arafathossain.baiust@gmail.com>
2023-11-16 10:56:26 +0600

    Done

--- src/Pages/Publications/CE.jsx
+++ src/Pages/Publications/CE.jsx
@@ -1,267 +1,55 @@
 import a from '../../../src/assets/Publications/CE/a.png'
 import b from '../../../src/assets/Publications/CE/b.png'
-import c from '../../../src/assets/Publications/CE/c.png'
-import d from '../../../src/assets/Publications/CE/d.png'
-import e from '../../../src/assets/Publications/CE/e.png'
-import f from '../../../src/assets/Publications/CE/f.jpg'
-import g from '../../../src/assets/Publications/CE/g.jpg'
-import h from '../../../src/assets/Publications/CE/h.png'
-import i from '../../../src/assets/Publications/CE/i.jpg'
-import j from '../../../src/assets/Publications/CE/j.jpg'
-import k from '../../../src/assets/Publications/CE/k.jpg'
-import l from '../../../src/assets/Publications/CE/l.png'
-import m from '../../../src/assets/Publications/CE/m.jpg'
-import n from '../../../src/assets/Publications/CE/n.png'
+// import c from '../../../src/assets/Publications/CE/c.png'
+// import d from '../../../src/assets/Publications/CE/d.png'
+// import e from '../../../src/assets/Publications/CE/e.png'
+// import f from '../../../src/assets/Publications/CE/f.jpg'
+// import g from '../../../src/assets/Publications/CE/g.jpg'
+// import h from '../../../src/assets/Publications/CE/h.png'
+// import i from '../../../src/assets/Publications/CE/i.jpg'
+// import j from '../../../src/assets/Publications/CE/j.jpg'
+// import k from '../../../src/assets/Publications/CE/k.jpg'
+// import l from '../../../src/assets/Publications/CE/l.png'
+// import m from '../../../src/assets/Publications/CE/m.jpg'
+// import n from '../../../src/assets/Publications/CE/n.png'
+
+import FacultyBody from "../../components/FacultyBody/FacultyBody";
 
 const CE = () => {
     return (
-        <div>
-            
-            <div className='msc-container'>
-                <div>
-                    <img src={a} alt="" />
-                </div>
-                <section className='msc-container-text'>
-                    <div>
-                        <h1 className='text-header'>Md. Imam Hossain</h1>
-                        <h1>Assistant Professor & HOD</h1>
-                        <h2>BAIUST, Cumilla</h2>
-                        <small>head.ce@baiust.ac.bd  <br />
-                            01683812288
-                        </small> <br />
-                        <button className="btn btn-active btn-neutral mt-3">View More</button>
-                    </div>
-                </section>
-            </div>
+        <div className="grid md:grid-cols-2 sm:grid-cols-1">
+            <FacultyBody
+                img={a}
+                name={"Golam Moktader Nayeem"}
+                designation={"Associate Professor & HOD"}
+                email={"head.cse@baiust.ac.bd"}
+                phone={" 01717293250"}
+                fieldOfWork={<p><b>ROBOTIC PATH PLANNING 3D</b> Path Planning for Autonomous Unmanned Aerial Vehicle (UAV) <br />
+                    <b>ARTIFICIAL INTELLIGENCE</b> Metaheuristics Algorithms for Robotic Path Planning <br />
+                    <b>DATA SCIENCE</b> Data classification using different shallow Machine Learning algorithms
+                </p>}
+                education={<p></p>}
+                workExperience={<p></p>}
+            >
+            </FacultyBody>
 
-            <div className='msc-container'>
-                <div>
-                    <img src={b} alt="" />
-                </div>
-                <section className='msc-container-text'>
-                    <div>
-                        <h1 className='text-header'>Md. Ridwan Bin Alam</h1>
-                        <h1>Lecturer</h1>
-                        <h2>BAIUST, Cumilla</h2>
-                        <small>ridwan.ce@baiust.ac.bd <br />
-                            
-                        </small> <br />
-                        <button className="btn btn-active btn-neutral mt-3">View More</button>
-                    </div>
-                </section>
-            </div>
-
-            <div className='msc-container'>
-                <div>
-                    <img src={c} alt="" />
-                </div>
-                <section className='msc-container-text'>
-                    <div>
-                        <h1 className='text-header'>Ashraful Tamjeed</h1>
-                        <h1>Lecturer</h1>
-                        <h2>BAIUST, Cumilla</h2>
-                        <small>ashraful.ce@baiust.ac.bd  <br />
-                            
-                        </small> <br />
-                        <button className="btn btn-active btn-neutral mt-3">View More</button>
-                    </div>
-                </section>
-            </div>
-
-
-            <div className='msc-container'>
-                <div>
-                    <img src={d} alt="" />
-                </div>
-                <section className='msc-container-text'>
-                    <div>
-                        <h1 className='text-header'>Ann Nazmun Sakib</h1>
-                        <h1>Lecturer</h1>
-                        <h2>BAIUST, Cumilla</h2>
-                        <small>sakib.ce@baiust.ac.bd  <br />
-                            
-                        </small> <br />
-                        <button className="btn btn-active btn-neutral mt-3">View More</button>
-                    </div>
-                </section>
-            </div>
-
-
-            <div className='msc-container'>
-                <div>
-                    <img src={e} alt="" />
-                </div>
-                <section className='msc-container-text'>
-                    <div>
-                        <h1 className='text-header'>Arman Hossain Patwary</h1>
-                        <h1>Lecturer</h1>
-                        <h2>BAIUST, Cumilla</h2>
-                        <small>arman.ce@baiust.ac.bd <br />
-                            
-                        </small> <br />
-                        <button className="btn btn-active btn-neutral mt-3">View More</button>
-                    </div>
-                </section>
-            </div>
-
-
-            <div className='msc-container'>
-                <div>
-                    <img src={f} alt="" />
-                </div>
-                <section className='msc-container-text'>
-                    <div>
-                        <h1 className='text-header'>Farhan Fahad Tonmoy</h1>
-                        <h1>Lecturer</h1>
-                        <h2>BAIUST, Cumilla</h2>
-                        <small>farhan.ce@baiust.ac.bd <br />
-                            
-                        </small> <br />
-                        <button className="btn btn-active btn-neutral mt-3">View More</button>
-                    </div>
-                </section>
-            </div>
-
-
-            <div className='msc-container'>
-                <div>
-                    <img src={g} alt="" />
-                </div>
-                <section className='msc-container-text'>
-                    <div>
-                        <h1 className='text-header'>Fayzul Kabir</h1>
-                        <h1>Assistant Professor</h1>
-                        <h2>BAIUST, Cumilla</h2>
-                        <small>fkabir.ce@baiust.ac.bd  <br />
-                            
-                        </small> <br />
-                        <button className="btn btn-active btn-neutral mt-3">View More</button>
-                    </div>
-                </section>
-            </div>
-
-            <div className='msc-container'>
-                <div>
-                    <img src={h} alt="" />
-                </div>
-                <section className='msc-container-text'>
-                    <div>
-                        <h1 className='text-header'>Md. Abu Zafor</h1>
-                        <h1>Associate Professor </h1>
-                        <h2>BAIUST, Cumilla</h2>
-                        <small> <br />
-                            
-                        </small> <br />
-                        <button className="btn btn-active btn-neutral mt-3">View More</button>
-                    </div>
-                </section>
-            </div>
-
-            <div className='msc-container'>
-                <div>
-                    <img src={i} alt="" />
-                </div>
-                <section className='msc-container-text'>
-                    <div>
-                        <h1 className='text-header'>Md. Sabbir Ahmed Shourav</h1>
-                        <h1>Assistant Professor</h1>
-                        <h2>BAIUST, Cumilla</h2>
-                        <small>sabbir.ce@baiust.ac.bd <br />
-                            
-                        </small> <br />
-                        <button className="btn btn-active btn-neutral mt-3">View More</button>
-                    </div>
-                </section>
-            </div>
-
-            <div className='msc-container'>
-                <div>
-                    <img src={j} alt="" />
-                </div>
-                <section className='msc-container-text'>
-                    <div>
-                        <h1 className='text-header'>Md Manik Mia</h1>
-                        <h1>Assistant Professor</h1>
-                        <h2>BAIUST, Cumilla</h2>
-                        <small>manik.ce@baiust.ac.bd  <br />
-                            
-                        </small> <br />
-                        <button className="btn btn-active btn-neutral mt-3">View More</button>
-                    </div>
-                </section>
-            </div>
-
-            <div className='msc-container'>
-                <div>
-                    <img src={k} alt="" />
-                </div>
-                <section className='msc-container-text'>
-                    <div>
-                        <h1 className='text-header'>Shahrul Ibney Feroz</h1>
-                        <h1>Lecturer</h1>
-                        <h2>BAIUST, Cumilla</h2>
-                        <small>feroz.ce@baiust.ac.bd <br />
-                            
-                        </small> <br />
-                        <button className="btn btn-active btn-neutral mt-3">View More</button>
-                    </div>
-                </section>
-            </div>
-
-            <div className='msc-container'>
-                <div>
-                    <img src={l} alt="" />
-                </div>
-                <section className='msc-container-text'>
-                    <div>
-                        <h1 className='text-header'>Iftekhar Alam Dipta</h1>
-                        <h1>Lecturer</h1>
-                        <h2>BAIUST, Cumilla</h2>
-                        <small>dipta.ce@baiust.ac.bd <br />
-                            
-                        </small> <br />
-                        <button className="btn btn-active btn-neutral mt-3">View More</button>
-                    </div>
-                </section>
-            </div>
-
-            <div className='msc-container'>
-                <div>
-                    <img src={m} alt="" />
-                </div>
-                <section className='msc-container-text'>
-                    <div>
-                        <h1 className='text-header'>Tansir Zaman Asik</h1>
-                        <h1>Lecturer</h1>
-                        <h2>BAIUST, Cumilla</h2>
-                        <small>tansir_ce@baiust.ac.bd <br />
-                            
-                        </small> <br />
-                        <button className="btn btn-active btn-neutral mt-3">View More</button>
-                    </div>
-                </section>
-            </div>
-
-
-            <div className='msc-container'>
-                <div>
-                    <img src={n} alt="" />
-                </div>
-                <section className='msc-container-text'>
-                    <div>
-                        <h1 className='text-header'>Md. Atiqur Rahman</h1>
-                        <h1>Lecturer</h1>
-                        <h2>BAIUST, Cumilla</h2>
-                        <small>atiqur.ce@baiust.ac.bd  <br />
-                            
-                        </small> <br />
-                        <button className="btn btn-active btn-neutral mt-3">View More</button>
-                    </div>
-                </section>
-            </div>
-
-            
+            <FacultyBody
+                img={b}
+                name={"Golam Moktader Nayeem"}
+                designation={"Associate Professor & HOD"}
+                email={"head.cse@baiust.ac.bd"}
+                phone={" 01717293250"}
+                fieldOfWork={<p><b>ROBOTIC PATH PLANNING 3D</b> Path Planning for Autonomous Unmanned Aerial Vehicle (UAV) <br />
+                    <b>ARTIFICIAL INTELLIGENCE</b> Metaheuristics Algorithms for Robotic Path Planning <br />
+                    <b>DATA SCIENCE</b> Data classification using different shallow Machine Learning algorithms
+                </p>}
+                education={<p></p>}
+                workExperience={<p></p>}
+            >
+            </FacultyBody>
         </div>
+
+
     );
 };
 
